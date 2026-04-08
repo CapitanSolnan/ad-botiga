4707e30661a545f57b8d19b422823b84d980a0cd
CapitanSolnan <capisol23@gmail.com>
2026-04-08 23:39:04 +0200

    guardado de img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,6 +17,7 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -23,7 +25,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -35,10 +39,12 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item px-1"><a href="{{ url('/producte') }}" class="btn btn-outline-primary">Productes</a></li>
-                        <li class="nav-item px-1"><a href="{{ url('/carrito') }}" class="btn btn-outline-primary">Carrito</a></li>
+                        <li class="nav-item px-1"><a href="{{ url('/producte') }}"
+                                class="btn btn-outline-primary">Productes</a></li>
+                        <li class="nav-item px-1"><a href="{{ url('/carrito') }}"
+                                class="btn btn-outline-primary">Carrito</a></li>
 
- 
+
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -54,14 +60,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -77,22 +83,24 @@
         </nav>
         <nav>
             <div>
-                <ul class="navbar-nav ms-auto" style="position: absolute; bottom: 1%; right:0%" >
-                    <li class="nav-item px-1"><a href="{{ url('/producte/administrar') }}" class="btn btn-outline-primary">Administrar Productes (Solo DEV)</a></li>
+                <ul class="navbar-nav ms-auto" style="position: fixed; bottom: 1%; right: 0%;">
+                    <li class="nav-item px-1"><a href="{{ url('/producte/administrar') }} "
+                            class="btn btn-outline-primary"> Administrar Productes (Solo DEV)</a></li>
                 </ul>
-                
+
             </div>
         </nav>
-@if(session("success"))
-<div class="container">
-    <div class="alert alert-success" role="alert">
-        <p>{{ session("success") }}</p>
-    </div>
-</div>
-@endif
+        @if(session("success"))
+            <div class="container">
+                <div class="alert alert-success" role="alert">
+                    <p>{{ session("success") }}</p>
+                </div>
+            </div>
+        @endif
         <main class="py-4">
             @yield('content')
         </main>
     </div>
 </body>
+
 </html>
