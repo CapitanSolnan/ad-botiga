9ffe1cb4fa153b01ed2c30ec12be69fc95a3f92e
CapitanSolnan <capisol23@gmail.com>
2026-04-08 22:45:24 +0200

    appServiceProvider http->htpps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Paginator::useBootstrap();
+
+
+public function boot(): void
+{
+    Paginator::useBootstrap();
+
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 }
